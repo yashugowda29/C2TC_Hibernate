5ec08a0f063aab03bc6977b8e21b3abc04cde3f5
yashugowda29 <yashuvk97@gmail.com>
2022-11-03 16:38:05 +0530

    first commit

--- OneToOneUni/src/com/tns/entity/Address.java
+++ OneToOneUni/src/com/tns/entity/Address.java
@@ -7,10 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-//@Table(name="address")
 public class Address implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
