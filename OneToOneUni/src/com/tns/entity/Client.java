672e48dabb17f9b6ac3751d4c70cfe45b47d81db
yashugowda29 <yashuvk97@gmail.com>
2022-11-03 16:37:20 +0530

    first commit

---
+++ OneToOneUni/src/com/tns/entity/Client.java
@@ -0,0 +1,50 @@
+package com.tns.entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Student student = new Student();
+		student.setName("Yashu");
+		
+		Address homeaddress = new Address();
+		homeaddress.setStreet("ABC");
+		homeaddress.setCity("Bangalore");
+		homeaddress.setState("Karnataka");
+		homeaddress.setZipcode("560058");
+		
+		//Inject
+		student.setAddress(homeaddress);
+		
+		Student student1 = new Student();
+		student1.setName("Shre");
+		
+		Address homeaddress1 = new Address();
+		homeaddress1.setStreet("XYZ");
+		homeaddress1.setCity("Bangalore");
+		homeaddress1.setState("Karnataka");
+		homeaddress1.setZipcode("560058");
+		
+		student1.setAddress(homeaddress1);
+		
+		em.persist(student);
+		em.persist(student1);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Student data are added");
+		
+		em.close();
+		factory.close();
+		
+	}
+}
