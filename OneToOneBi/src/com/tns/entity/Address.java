7a7913bd44179785c93fdfa2b4f9f744afceb925
yashugowda29 <yashuvk97@gmail.com>
2022-11-04 15:23:56 +0530

    first commit

---
+++ OneToOneBi/src/com/tns/entity/Address.java
@@ -0,0 +1,79 @@
+package com.tns.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Address implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int address_id;
+	
+	@Column(name="address_street")
+	private String street;
+	
+	@Column(name="address_city")
+	private String city;
+	
+	@Column(name="address_state")
+	private String state;
+	
+	@Column(name="address_zipcode")
+	private String zipcode;
+	
+	@OneToOne(mappedBy="address")
+	private Student student;
+
+	public int getAddress_id() {
+		return address_id;
+	}
+
+	public void setAddress_id(int address_id) {
+		this.address_id = address_id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
